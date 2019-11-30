050294d074a28fe676aec3184a37b3d85360e8f2
Ran Le <ranle.private@gmail.com>
2019-11-30 00:53:58 -0800

    stop sign recog

--- asl_turtlebot/scripts/supervisor.py
+++ asl_turtlebot/scripts/supervisor.py
@@ -184,7 +184,7 @@ class Supervisor:
         """ sends zero velocity to stay put """
 
         vel_g_msg = Twist()
-	vel_g_msg.linear.x = 0
+        vel_g_msg.linear.x = 0
         vel_g_msg.angular.z = 0
         self.cmd_vel_publisher.publish(vel_g_msg)
 
@@ -260,10 +260,10 @@ class Supervisor:
 
         elif self.mode == Mode.STOP:
             # At a stop sign
-	    if self.has_stopped():
-	        self.init_crossing()
-	    else:
-		self.stay_idle()
+            if self.has_stopped():
+                self.init_crossing()
+            else:
+                self.stay_idle()
 
         elif self.mode == Mode.CROSS:
             # Crossing an intersection
